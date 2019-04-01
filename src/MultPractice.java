b33e5572801dfe676fd9ad04b1f63d1111f86d56
super <f>
2019-04-01 18:59:42 -0400

    f

--- src/MultPractice.java
+++ src/MultPractice.java
@@ -1,5 +1,23 @@
 /**
  * Created by Teacher on 4/1/2019.
  */
-public class MultPractice {
+public class MultPractice implements StudyPractice
+{
+    private int f;
+    private int s;
+    public MultPractice(int firstInt, int secInt)
+    {
+        f = firstInt;
+        s = secInt;
+
+    }
+    public String getProblem()
+    {
+        String problem = f + " TIMES " + s;
+        return problem;
+    }
+    public void nextProblem()
+    {
+        s++;
+    }
 }
